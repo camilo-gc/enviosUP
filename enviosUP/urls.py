ce339c670c6caebc4ede4c0c110ae984f992056d
camilo-gc <camilogarcia.dev@gmail.com>
2020-04-11 15:37:25 -0500

    funcionando

--- enviosUP/urls.py
+++ enviosUP/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from empresa import views
 from personal.views import registroCliente
+from django.contrib.auth.views import login_required, logout_then_login
 
 urlpatterns = [
     path('', views.index),
