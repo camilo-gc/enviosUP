09e97b22cd43e54e0357ae61695c8fba38e289e8
camilo-gc <camilogarcia.dev@gmail.com>
2020-04-25 18:28:20 -0500

    modificacion en autenticacion

--- personal/urls.py
+++ personal/urls.py
@@ -4,5 +4,7 @@ from personal import views
 
 urlpatterns = [
     path('registroEmpleado/', views.registroEmpleado, name='registrarEmpleado'),
-    path('login/', views.login_user, name= 'inicio_sesion'),
+    path('validacion/', views.login_user, name= 'inicio_sesion'),
+    path('validacion/', views.login_user, name= 'validacion'),
+    path('logout/', views.logout, name= 'logout'),
 ]
--- personal/views.py
+++ personal/views.py
@@ -8,6 +8,8 @@ from empresa.models import Sucursal
 from personal.models import Tipo_documento, Rol, Sucursal
 
 # Create your views here.
+
+
 def registroEmpleado(request):
     tiposDoc = Tipo_documento.objects.all()
     roles = Rol.objects.all()
@@ -28,64 +30,66 @@ def registroEmpleado(request):
         inicioContrato = request.POST.get('fecha_inicio', None)
         finContrato = request.POST.get('fecha_fin', None)
         salario = request.POST.get('salario', None)
-        existe = Persona.objects.filter(per_documento = documento).exists()
+        existe = Persona.objects.filter(per_documento=documento).exists()
         try:
             if(existe):
                 messages.info(request, 'Ya se encuentra registrado.')
             else:
                 persona = Persona(per_documento=documento, per_nombre=nombre, per_apellido=apellido, per_fecha_naci=fecha_nac,
-                            per_email=correo, per_telefono=telefono, per_celular=celular, per_direccion=direccion, td_id_id=tipo_doc, per_contrasena=contrasena)
+                                  per_email=correo, per_telefono=telefono, per_celular=celular, per_direccion=direccion, td_id_id=tipo_doc, per_contrasena=contrasena)
                 persona.save()
 
                 empleado = Empleado(per_documento_id=documento, suc_id_id=sucursal, rol_id_id=rol, emp_inicio_contrato=inicioContrato,
-                            emp_fin_contrato=finContrato, emp_salario=salario, emp_estado=1)
+                                    emp_fin_contrato=finContrato, emp_salario=salario, emp_estado=1)
                 empleado.save()
 
                 messages.success(request, 'Registro exitoso.')
-        except:''
+        except:
             messages.error(request, 'Ocurrio un error.')
-    else:''
+    else:
+        ''
 
-    return render(request, 'registroEmpleado.html', {'tipos': tiposDoc, 'roles':roles, 'suc':sucursales})
+    return render(request, 'registroEmpleado.html', {'tipos': tiposDoc, 'roles': roles, 'suc': sucursales})
 
 
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            if request.user.is_staff is not False:
-                print('Administrador')
-                return redirect(reverse_lazy('administracion'))#Redirecciona panel admin #acordar lo de app_name para la rutas
-            else:
-                #Se deve validar que rol tiene el usuario para redireccionarlo despues
-                print('empleado')
-                return redirect(reverse_lazy('operacion'))#Redireccionar Panel NO administrador
+
+    username = request.POST.get('username', None)
+    password = request.POST.get('password', None)
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        if request.user.is_staff is not False:
+            # Redirecciona panel admin #acordar lo de app_name para la rutas
+            return redirect(reverse_lazy('administracion'))
         else:
-            print('credenciales incorrectas')
-            messages.error(request, 'Usuario o contraseña incorrectos')
-            return render(request, 'empresa/index.html')#De prueba, se debe modificar.
-#    if request.user.is_active:
-#        return redirect(reverse_lazy('administracion'))
-#    else:
-#        return render(request, empresa/index.html') #Probaaaaaar!!!
+            # Se deve validar que rol tiene el usuario para redireccionarlo despues
+            # Redireccionar Panel NO administrador
+            return redirect(reverse_lazy('operacion'))
+    else:
+        print('credenciales incorrectas')
+        messages.error(request, 'Usuario o contraseña incorrectos')
+        # De prueba, se debe modificar.
+        return render(request, 'empresa/index.html')
+
 
 def logout(request):
     # Finalizamos la sesión
     do_logout(request)
     # Redireccionamos a la portada
-    return redirect(reverse_lazy('home'))
+    return redirect(reverse_lazy('login'))
+
 
 def codificarContrasena(cadena):
-    b = cadena.encode("UTF-8") # codificando la cadena en bytes
-    e = b64encode(b) # codificar los bytes Base64
-    con = e.decode("UTF-8") # decodificando los bytes Base64 a cadena
+    b = cadena.encode("UTF-8")  # codificando la cadena en bytes
+    e = b64encode(b)  # codificar los bytes Base64
+    con = e.decode("UTF-8")  # decodificando los bytes Base64 a cadena
     return con
 
+
 def decodificarContrasena(cadena):
-    b1 = cadena.encode("UTF-8")# codificar la cadena codificaada Base64 en bytes
-    d = b64decode(b1)# decodificando los bytes Base64
-    con2 = d.decode("UTF-8") # decodificando los bytes a cadena
+    # codificar la cadena codificaada Base64 en bytes
+    b1 = cadena.encode("UTF-8")
+    d = b64decode(b1)  # decodificando los bytes Base64
+    con2 = d.decode("UTF-8")  # decodificando los bytes a cadena
     return con2
